2035c04ea742444b769d9dd81420f4410b8cb131
Husnain_Khurshid <muhammadhusnainkh@gmail.com>
2022-12-20 15:15:40 +0500

    fix: (writed README and setup file)

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Hawqal-python
+
+![](http://ForTheBadge.com/images/badges/made-with-python.svg)
+
+# Hawqal
+
+Python package that contains the data of world's countries,states and their cities name
+
+## Functions
+
+- Get Countries
+- Get Cities By State
+- Get State By Country
+- Get Cities By Country
+
+
+## Usage/Examples
+
+```python
+from hawqal.country import Country
+Country.getCountries()
+
+from hawqal.city import City
+City.getCities()
+
+from hawqal.states import StatesByCountry
+StatesByCountry.getStates()
+
+```
+
+## Success Response
+
+- ['Afghanistan', 'Aland Islands', 'Albania', 'Algeria', . . . ]
+- ['Alabama', 'Alaska', 'American Samoa', 'Arizona', . . . ]
+
+## Error Response
+
+- [ ] 
+## Tech Stack
+
+**Client:** Python 3.10.9
+
+
+## Authors
+
+- [Capregsoft](https://www.github.com/capregsoft)
+- [Husnain Khurshid](https://www.github.com/husnain9)
+
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 """Python setup.py for hawqal package"""
-from setuptools import find_packages, setup
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name="hawqal",
     version="0.1.0",
-    description="Python package that contains the data of world's countries,states and their cities name",
+    author="Husnain Khurshid",
+    author_email="muhammadhusnainkh@gmail.com",
+    description=(
+        "Python package that contains the data of world's countries,states and their cities name"),
+    license="BSD",
+    keywords="atlas documentation tutorial",
     url="https://github.com/CapregSoft/Hawqal-python.git",
-    long_description_content_type="text/markdown",
-    author="capregsoft",
-    packages=find_packages(exclude=["tests", ".github"]),
-    entry_points={
-        "console_scripts": ["hawqal = hawqal.__main__:main"]
-    },
+    packages=['hawqal'],
+    long_description=read('README.md'),
 )
