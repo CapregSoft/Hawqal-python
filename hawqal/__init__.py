8cc2ee469b525e50c60f48e48c7227679cd74764
Husnain_Khurshid <muhammadhusnainkh@gmail.com>
2023-01-04 16:28:35 +0500

    fix: (fixed state function)

--- hawqal/__init__.py
+++ hawqal/__init__.py
@@ -5,5 +5,6 @@ from hawqal.states import States
 getCities = City.getCities
 getCountries = Country.getCountries
 getStates = States.getStates
+getState = States.getState
 
-__all__ = [getCities, getCountries, getStates]
+__all__ = [getCities, getCountries, getStates, getState]
--- hawqal/states.py
+++ hawqal/states.py
@@ -28,30 +28,30 @@ class States:
 
         query = "SELECT"
 
-        if len(meta)>0:
+        if len(meta) > 0:
             selectedFields = Filter.StateFilter(meta)
             query = query + f' {selectedFields}'
-        elif len(meta)==0:
+        elif len(meta) == 0:
             query = query + " *"
 
         query = query + " FROM states"
 
-        if len(country_name)>0:
-            query=query+ f' where country_name= "{string.capwords(country_name)}"'
+        if len(country_name) > 0:
+            query = query + \
+                f' where country_name= "{string.capwords(country_name)}"'
 
         cursor.execute(query)
 
         data_json = []
-        header = [i[0] for i in curr.description]
+        header = [i[0] for i in cursor.description]
         data = cursor.fetchall()
         for i in data:
             data_json.append(dict(zip(header, i)))
         return data_json
 
-
     @staticmethod
-    def getState(country_name="",state_name="", meta={}):
-        
+    def getState(country_name="", state_name="", meta={}):
+
         file_name = os.path.join(
             os.path.dirname(__file__), '..', 'database', 'hawqalDB.sqlite')
 
@@ -61,20 +61,20 @@ class States:
 
         query = "SELECT"
 
-        if len(meta)>0:
+        if len(meta) > 0:
             selectedFields = Filter.StateFilter(meta)
             query = query + f' {selectedFields}'
-        elif len(meta)==0:
+        elif len(meta) == 0:
             query = query + " *"
 
         query = query + " FROM states where"
 
-        if len(country_name)>0 and len(state_name)>0:
+        if len(country_name) > 0 and len(state_name) > 0:
             query = f' country_name= "{string.capwords(country_name)}" and state_name= "{string.capwords(state_name)}"'
-        elif len(country_name)>0:
-            query=query+ f' country_name= "{string.capwords(country_name)}"'
-        elif len(state_name)>0:       
-            query=query+ f' state_name= "{string.capwords(state_name)}"'
+        elif len(country_name) > 0:
+            query = query + f' country_name= "{string.capwords(country_name)}"'
+        elif len(state_name) > 0:
+            query = query + f' state_name= "{string.capwords(state_name)}"'
 
         query = query + " ORDER BY country_name"
 
