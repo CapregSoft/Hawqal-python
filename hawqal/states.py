97af1289d5548edfbcc16089fe7da67fd199bec8
hamzaanis <hamzaanis9514@gmail.com>
2023-01-04 15:50:09 +0500

    Get state and get states

--- hawqal/states.py
+++ hawqal/states.py
@@ -7,7 +7,7 @@ import os
 class States:
 
     @staticmethod
-    def getStates(country="", meta={}):
+    def getStates(country_name="", meta={}):
         """
             1. States function takes two parameters as input state name and filters.\n
             2. By default, function will return states name.\n
@@ -26,35 +26,61 @@ class States:
             database = Database(file_name).makeConnection()
             cursor = database.cursor()
 
-        if country == "" and len(meta) == 0:
-            data = cursor.execute(
-                f'SELECT state_name FROM states')
-            return [state[0] for state in data]
+        query = "SELECT"
 
-        elif country == "" and len(meta) != 0:
+        if len(meta)>0:
             selectedFields = Filter.StateFilter(meta)
-            if len(selectedFields) != 0:
-                data = cursor.execute(
-                    f'SELECT state_name,{selectedFields} FROM states')
-                return [list(state) for state in data]
-            else:
-                data = cursor.execute(
-                    f'SELECT state_name FROM states')
-                return [state for state in data]
+            query = query + f' {selectedFields}'
+        elif len(meta)==0:
+            query = query + " *"
 
-        elif country != "" and len(meta) > 0:
-            selectedFields = Filter.StateFilter(meta)
-            if len(selectedFields) != 0:
-                data = cursor.execute(
-                    f'SELECT state_name,{selectedFields} FROM states WHERE country_name = "{string.capwords(country)}"')
-                return [list(state) for state in data]
-            else:
-                data = cursor.execute(
-                    f'SELECT state_name FROM states WHERE country_name = "{string.capwords(country)}"')
-                return [state[0] for state in data]
+        query = query + " FROM states"
 
-        elif country != "" and len(meta) == 0:
+        if len(country_name)>0:
+            query=query+ f' where country_name= "{string.capwords(country_name)}"'
+
+        cursor.execute(query)
+
+        data_json = []
+        header = [i[0] for i in curr.description]
+        data = cursor.fetchall()
+        for i in data:
+            data_json.append(dict(zip(header, i)))
+        return data_json
+
+
+    @staticmethod
+    def getState(country_name="",state_name="", meta={}):
+        
+        file_name = os.path.join(
+            os.path.dirname(__file__), '..', 'database', 'hawqalDB.sqlite')
+
+        with open(file_name, 'r', encoding="utf8") as db:
+            database = Database(file_name).makeConnection()
+            cursor = database.cursor()
+
+        query = "SELECT"
+
+        if len(meta)>0:
             selectedFields = Filter.StateFilter(meta)
-            data = cursor.execute(
-                f'SELECT state_name FROM states WHERE country_name = "{string.capwords(country)}"')
-            return [state[0] for state in list(data)]
+            query = query + f' {selectedFields}'
+        elif len(meta)==0:
+            query = query + " *"
+
+        query = query + " FROM states where"
+
+        if len(country_name)>0 and len(state_name)>0:
+            query = f' country_name= "{string.capwords(country_name)}" and state_name= "{string.capwords(state_name)}"'
+        elif len(country_name)>0:
+            query=query+ f' country_name= "{string.capwords(country_name)}"'
+        elif len(state_name)>0:       
+            query=query+ f' state_name= "{string.capwords(state_name)}"'
+
+        query = query + " ORDER BY country_name"
+
+        data_json = []
+        header = [i[0] for i in curr.description]
+        data = cursor.fetchall()
+        for i in data:
+            data_json.append(dict(zip(header, i)))
+        return data_json
